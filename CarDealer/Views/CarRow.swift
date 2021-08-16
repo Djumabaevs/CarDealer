c8518b68b89e4f54bf97d16051ba82e9c62426ae
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-16 15:51:51 +0600

    create car row new swift ui file

---
+++ CarDealer/Views/CarRow.swift
@@ -0,0 +1,20 @@
+//
+//  CarRow.swift
+//  CarDealer
+//
+//  Created by Bakyt Dzhumabaev on 16/8/21.
+//
+
+import SwiftUI
+
+struct CarRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CarRow()
+    }
+}
